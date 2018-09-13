0dc4622d43b3cf7df353fe1e8a2aac5731873939
O M E N <O M E N@LAPTOP-PJ10TE7S>
2018-09-13 13:50:57 +0700

    Initial Commit

---
+++ src/dynamicprogram/Latihan1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dynamicprogram;
+import java.math.BigInteger;
+import java.util.Scanner;
+/**
+ *
+ * @author O M E N
+ */
+public class Latihan1 {
+    private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, ,1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    public static void main(String[] args) {
+        
+    String identitas = "Salman Naufal Haq / XR2 / 31";
+    tampilJudul(identitas);
+    }
+}
