e06ef351d10043fd326f07b613cfb5b32cd406d5
O M E N <O M E N@LAPTOP-PJ10TE7S>
2018-09-13 13:56:31 +0700

    Tambah Tampil Hasil

--- src/dynamicprogram/Latihan1.java
+++ src/dynamicprogram/Latihan1.java
@@ -24,6 +24,7 @@ public class Latihan1 {
     tampilJudul(identitas);
     int n = tampilInput();
     BigInteger hasil = fibo(n);
+    tampilHasil(n, hasil);
     }
     
     private static int tampilInput()
@@ -45,4 +46,9 @@ return n;
         }
      return hasil[n-1];
 }
+    
+    private static void tampilHasil(int n, BigInteger hasil)
+    {
+        System.out.println("Bilangan Fibonacci ke-" +n+" : " + hasil);
+    }
 }
